32c458a42a6d9b5c02d1c53904493134ca021027
JulianaDamiano <64518589+JulianaDamiano@users.noreply.github.com>
2021-03-04 12:54:01 -0500

    Add files via upload

---
+++ portada.js
@@ -0,0 +1,21 @@
+var cursor;
+
+var Portada ={
+	preload: function () {
+		juego.load.image('port','img/portada.png');
+
+
+	},
+
+	create: function(){
+
+		juego.add.tileSprite(0,0,400,540,'port');
+       
+	},
+
+	update: function(time,delta){
+		
+				
+	}
+   
+};
